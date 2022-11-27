541f261c7c35e8ab3d614d6a90e5d6c8b97b3ebb
raja palai <raja.palai.1992@gmail.com>
2022-11-27 13:51:59 +0530

    log file

--- src/main/java/com/employeeservice/employeeappnew/EmployeeServiceEmployeeappnewApplication.java
+++ src/main/java/com/employeeservice/employeeappnew/EmployeeServiceEmployeeappnewApplication.java
@@ -1,9 +1,12 @@
 package com.employeeservice.employeeappnew;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "employee_api",version = "3.0",description = "Employee microservice"))
 public class EmployeeServiceEmployeeappnewApplication {
 
 	public static void main(String[] args) {
